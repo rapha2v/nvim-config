501c795ea5b9c8e37e4b9f2d89350558e0687fac
Raphael Porto <raphael_port@outlook.com>
2024-04-24 02:00:11 -0300

    feat: first commit

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    config = function()
+      require("catppuccin").setup({
+        flavour = "macchiato",
+        no_italic = false,
+        no_bold = false,
+        no_underline = false,
+        transparent_background = true,
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
